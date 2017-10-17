e2b424ee0016cdb81241d0432d2c283c07050019
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2017-10-17 10:41:08 +0530

    More user-friendly command-line usage (#5)
    * Remove unnecessary headers
    
    * Improve command-line usage
    
    * Remove unused import time
    
    * Add custom RGB colors to generated text
    
    * Remove unnecessary line breaks
    
    * Specify handwriting style
    
    * Some code cleanup
    
    * Fix color adding when reading from file
    
    * Update usage and examples
    
    * Bolden example sub-headings
    
    * Delete example.png

--- README.md
+++ README.md
@@ -9,17 +9,46 @@ generate handwritten homework for given text.
 Usage
 -----
 
-1. Create a plaintext file with no more than 100 chars on each line.(we call it sample.txt)
-2. Navigate to the folder with the script.
-3. `mkdir images pages`
-4. `python get_hand.py sample.txt`
-5. `python make_page.py`
+```
+usage: get_hand.py [-h] (-f FILE | -t TEXT) [-s STYLE] [-b BIAS] [-p POSITION]
+                   [-c COLOR]
 
+Plain text to handwriting generator.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f FILE, --file FILE  text file to generate handwriting from (default: None)
+  -t TEXT, --text TEXT  text to generate handwriting from (default: None)
+  -s STYLE, --style STYLE
+                        handwriting style from 0 to 5, where 5 is random style
+                        (default: 5)
+  -b BIAS, --bias BIAS  handwriting bias from 0.1 to 1 (default: 0.8)
+  -p POSITION, --position POSITION
+                        start at specific position (default: 0)
+  -c COLOR, --color COLOR
+                        add custom RGB color values to generated text
+                        (default: 0,0,0)
+```
 
 Example
 -------
 
-![Example of handwriting](example.png)
+**Generating handwritten text page from a file (with no more than 100 characters in each line) with handwriting style 4:**
+```
+python get_hand.py -f desc.txt -s 4
+python make_page.py
+```
+Now open `pages/1.png`:
+
+![sample handwriting](https://i.imgur.com/9SPwYnd.png)
+
+**Generating handwritten text using a string in blue color & custom bias with handwriting style 2:**
+```
+python get_hand.py -t "Homework could have been easier" -s 2 -c 0,0,200 -b 0.85
+```
+Now open `images/0.png`:
+
+![blue ink](https://i.imgur.com/IrGDNx3.png)
 
 
 More Info
--- get_hand.py
+++ get_hand.py
@@ -1,53 +1,111 @@
+import argparse
 import os
 import sys
 import base64
 import requests
+from PIL import Image
 
-def get_handwriting(text, bias=0.8, samples=1):
+
+def get_arguments():
+    parser = argparse.ArgumentParser(
+        description='Plain text to handwriting generator.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    group = parser.add_mutually_exclusive_group(required=True)
+
+    group.add_argument(
+        '-f', '--file', help='text file to generate handwriting from')
+    group.add_argument(
+        '-t', '--text', help='text to generate handwriting from')
+    parser.add_argument(
+        '-s', '--style', help='handwriting style from 0 to 5, where 5 is random style',
+        type=int, default=5)
+    parser.add_argument(
+        '-b', '--bias', help='handwriting bias from 0.1 to 1',
+        type=float, default=0.8)
+    parser.add_argument(
+        '-p', '--position', help='start at specific position',
+        type=int, default=0)
+    parser.add_argument(
+        '-c', '--color', help='add custom RGB color values to generated text',
+        type=str, default='0,0,0')
+
+    return parser.parse_args()
+
+
+def get_handwriting(text, style, bias, samples=1):
+    style_models = (
+            '../data/trainset_diff_no_start_all_labels.nc,973+905',
+            '../data/trainset_diff_no_start_all_labels.nc,1082+554',
+            '../data/trainset_diff_no_start_all_labels.nc,1495+898',
+            '../data/trainset_diff_no_start_all_labels.nc,1970+378',
+            '../data/trainset_diff_no_start_all_labels.nc,1561+872',
+            '')
+
     payload = {'text': text,
-               'style': '../data/trainset_diff_no_start_all_labels.nc,1082+554',
+               'style': style_models[style],
                'bias': bias,
                'samples': samples}
-    headers = {'Host': 'www.cs.toronto.edu',
-               'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:43.0) Gecko/20100101 Firefox/43.0',
-               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-               'Referrer': 'http://www.cs.toronto.edu/~graves/handwriting.cgi',
-               'Connection': 'keep-alive',
-               'Accept-Language': 'en-US,en;q=0.5',
-               'Accept-Encoding': 'gzip, deflate'}
     url = 'http://www.cs.toronto.edu/~graves/handwriting.cgi'
-    page = requests.get(url, headers=headers, params=payload)
+    page = requests.get(url, params=payload)
     text = page.text
     print('.', end='')
+    # print('.', end='')
     search_string = '<img src="data:image/jpeg;base64,'
     start = text.find(search_string) + len(search_string)
     image_str = text[start:-5]
     return image_str
 
 
-with open(sys.argv[1], 'r') as fl:
-    lines = [i.strip() for i in fl.readlines()]
+def add_color(color, image_out):
+    img = Image.open(image_out)
+    width, height = img.size
+    for x in range(width):
+        for y in range(height):
+            old_color = list(img.getpixel((x, y)))
+            new_color = [old_color[x] + color[x]
+                         for x in range(3)]
+            img.putpixel((x, y), tuple(new_color))
+    img.save(image_out)
 
-start_at = 0
-try:
-    start_at = int(sys.argv[2])
-except IndexError:
-    pass
 
-if not os.path.exists('images'):
-    print('Creating images folder')
-    os.makedirs('images')
+def command_line():
+    args = get_arguments()
 
- 
+    if args.file:
+        with open(args.file, 'r') as fl:
+            text_in = [i.strip() for i in fl.readlines()]
+    elif args.text:
+        text_in = [args.text]
 
-print('Starting handwriting generation')
-for index, line in enumerate(lines):
-    if index < start_at:
-        continue
-    if line.strip() != '':
-        print(index, ' - ', line, end='')
-        image_str = get_handwriting(line)
-        x = base64.b64decode(image_str)
-        with open('images/'+str(index)+'.png', 'wb') as fl:
-            fl.write(x)
-        print('|')
+    style = args.style
+    start_at = args.position
+    bias = args.bias
+
+    color = args.color
+    color = color.replace(' ', '')
+    color = color.split(',')
+    color = list(map(int, color))
+
+    if not os.path.exists('images'):
+        print('Creating images folder')
+        os.makedirs('images')
+
+    print('Starting handwriting generation')
+    for index, line in enumerate(text_in):
+        if index < start_at:
+            continue
+        if not line.strip() == '':
+            print(index, ' - ', line, end='')
+            image_str = get_handwriting(line, style, bias)
+            x = base64.b64decode(image_str)
+            image_out = os.path.join('images', str(index) + '.png')
+            with open(image_out, 'wb') as fl:
+                fl.write(x)
+            if not color == [0, 0, 0]:
+                add_color(color, image_out)
+            print('|')
+
+
+if __name__ == '__main__':
+
+    command_line()
